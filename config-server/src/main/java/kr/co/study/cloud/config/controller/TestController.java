48e91d8ee6393adc21019f16fc59ad38a6a74fb6
Wally <>
2022-11-11 09:52:07 +0900

    eureka

--- config-server/src/main/java/kr/co/study/cloud/config/controller/TestController.java
+++ config-server/src/main/java/kr/co/study/cloud/config/controller/TestController.java
@@ -1,4 +1,4 @@
-package kr.co.study.cloud.configserver.controller;
+package kr.co.study.cloud.config.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
